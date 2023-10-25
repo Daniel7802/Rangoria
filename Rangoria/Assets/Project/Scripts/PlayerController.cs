31ddf261d40ffca20410992e0cfa4d04c136ccec
AjandroMLG <alejandro.martinez.membrilla@enti.cat>
2023-10-25 23:08:42 +0200

    perdonCabeza

--- Rangoria/Assets/Project/Scripts/PlayerController.cs
+++ Rangoria/Assets/Project/Scripts/PlayerController.cs
@@ -7,31 +7,32 @@ public class PlayerController : MonoBehaviour
     // BOOMERANG VARIABLES
     public bool isThrown = false;
 
-//    [SerializeField]
-//    GameObject boomerang;
+    //    [SerializeField]
+    //    GameObject boomerang;
 
-//    public Vector3 aimPoint;
+    //    public Vector3 aimPoint;
 
-//    private BoomerangMovement boomerangScript;
+    //    private BoomerangMovement boomerangScript;
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//        boomerangScript = boomerang.GetComponent<BoomerangMovement>();
-//    }
+    //    // Start is called before the first frame update
+    //    void Start()
+    //    {
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            if(!isThrown) 
-//            {
-//                boomerang.SetActive(true);
-//                aimPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//                isThrown = true;
-//            }
-//        }
-//    }
-//}
+    //        boomerangScript = boomerang.GetComponent<BoomerangMovement>();
+    //    }
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (Input.GetMouseButtonDown(0))
+    //        {
+    //            if(!isThrown) 
+    //            {
+    //                boomerang.SetActive(true);
+    //                aimPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //                isThrown = true;
+    //            }
+    //        }
+    //    }
+    //}
+}
